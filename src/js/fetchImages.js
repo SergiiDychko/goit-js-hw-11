6c6e61d043247f467d4a672e53513beb3af4c3a0
Sergii Dychko <110810294+SergiiDychko@users.noreply.github.com>
2022-10-26 22:10:41 +0300

    hw done

--- src/js/fetchImages.js
+++
@@ -1,5 +0,0 @@
-export async function fetchImages(request) {
-    return await fetch(
-      `https://pixabay.com/api/?key=30849691-d4b4d8699eb6bab45d758087e&q=${request}&image_type=photo&orientation=horizontal&safesearch=true`
-    );
-}
